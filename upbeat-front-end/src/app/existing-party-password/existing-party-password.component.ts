7e2869f8635c4465c1c909615665509be429facb
EEscalas <elena@agonswim.com>
2018-03-06 13:42:53 -0800

    guest password page identifies partyname from url

--- upbeat-front-end/src/app/existing-party-password/existing-party-password.component.ts
+++ upbeat-front-end/src/app/existing-party-password/existing-party-password.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-existing-party-password',
   templateUrl: './existing-party-password.component.html',
   styleUrls: ['./existing-party-password.component.css']
 })
+
 export class ExistingPartyPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+
+  partyName: string;
 
   ngOnInit() {
+    this.partyName = this.route.snapshot.paramMap.get('name');
   }
 
 }
