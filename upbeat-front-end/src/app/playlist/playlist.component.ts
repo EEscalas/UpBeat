61d5e5426451b452b0085c7eaa9a106b5b3e18ed
Michelle <michelleklam@ucla.edu>
2018-03-08 13:47:32 -0800

    added sorting function, sorts songs in decreasing order by upcount

--- upbeat-front-end/src/app/playlist/playlist.component.ts
+++ upbeat-front-end/src/app/playlist/playlist.component.ts
@@ -1,21 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import {Song, SONGS} from '../song'
-import {ActivatedRoute, Router} from '@angular/router'
+import {Song, SONGS} from '../song';
+import {ActivatedRoute, Router} from '@angular/router';
+
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.css']
 })
+
+// @Pipe({
+//   name:"sort"
+// })
+
+
+
+// export class SortPipe implements PipeTransform {
+//   transform(array: this.songs): Array<Song> {
+//     array.sort((a: any, b: any) => {
+//       if (a.upcount < b. upcount) {
+//         return -1;
+//       } else if (a.upcount > b.upcount) {
+//         return 1;
+//       } else return 0;
+//     }
+//     return array;
+//   }
+// }
+
 export class PlaylistComponent implements OnInit {
 
   constructor(private route:ActivatedRoute, private router:Router) { }
   partyName :string;
   partyid :number;
   //use partyid to get songs
-  songs = SONGS;
+  //songs = SONGS;
+  songs : Song[];
   isDj: boolean = false;
   url: string;
   ngOnInit() {
+    this.songs = SONGS;
+    this.songs = this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
+
     this.partyName = this.route.snapshot.paramMap.get('name');
   	this.partyid = +this.route.snapshot.paramMap.get('id');
     //this.songs.OrderBy(x=>x.upcount);
@@ -27,9 +60,37 @@ export class PlaylistComponent implements OnInit {
   }
   onSelectUpVote(song:Song) :void {
   	song.upcount++;
+    this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
   }
   onSelectDownVote(song:Song) :void {
   	song.upcount--;
+    this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
   }
 
+//   sort((obj1:Song, obj2:Song) => {
+//     if(obj1.upcount > obj2.upcount){
+//       return 1;
+//     }
+//     if(obj1.upcount < obj2.upcount){
+//       return -1;
+//     }
+//     return 0;
+//   });
+// }
 }
