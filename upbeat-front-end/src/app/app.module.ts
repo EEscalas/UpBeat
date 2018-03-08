761d16b4a4a74dd83a77ffc3d1fd1d234d5772a3
Michelle <michelleklam@ucla.edu>
2018-03-07 22:24:58 -0800

    missed another

--- upbeat-front-end/src/app/app.module.ts
+++ upbeat-front-end/src/app/app.module.ts
@@ -3,32 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PartyListComponent } from './party-list/party-list.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ExistingPartyPasswordComponent } from './existing-party-password/existing-party-password.component';
-<<<<<<< HEAD
 import { PlaylistComponent } from './playlist/playlist.component';
 import { CreatePartyComponent } from './create-party/create-party.component';
 import { DjPlaylistComponent } from './dj-playlist/dj-playlist.component';
-=======
-import { DjLoginComponent } from './dj-login/dj-login.component';
->>>>>>> e474e020eafbe93a76e37e1703a0c5f7edb1b10c
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PartyListComponent,
     ExistingPartyPasswordComponent,
-<<<<<<< HEAD
     PlaylistComponent,
     CreatePartyComponent,
     DjPlaylistComponent
-=======
-    DjLoginComponent
->>>>>>> e474e020eafbe93a76e37e1703a0c5f7edb1b10c
   ],
   imports: [
     BrowserModule,
