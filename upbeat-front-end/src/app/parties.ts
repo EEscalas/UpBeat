17c462bd97c59b78b55570b9abfb398865a9eb77
EEscalas <elena@agonswim.com>
2018-03-13 15:57:41 -0700

    Michelle css work from meeting today #babysteps

--- upbeat-front-end/src/app/parties.ts
+++ upbeat-front-end/src/app/parties.ts
@@ -1,14 +1,14 @@
 export class Party {
     constructor(
         public id: number = null,
-        public name: string = "",//,
-        public password: string = "",
-        public accessKey: string = ""
+        public name: string = "",
+        public accessKey: string = "",
+        public password: string = ""
     ){}
 }
 
 
-// export const PARTIES: Party[] =[
+//  export const PARTIES: Party[] =[
 //     { name: "lit", id : 1 },
 //     { name: "lit part 2", id : 2 },
 //     { name: "fire emoji", id : 3 }
--- upbeat-front-end/src/app/playlist/playlist.component.ts
+++ upbeat-front-end/src/app/playlist/playlist.component.ts
@@ -14,26 +14,48 @@ import { Party } from '../parties';
 export class PlaylistComponent implements OnInit {
 
   constructor(private route:ActivatedRoute, private router:Router, private songService: SongService, private apiService: ApiService) { }
-  // Initialize variables
   partyName :string;
   partyid :number;
+  //use partyid to get songs
+  //songs = SONGS;
   songs : Song[] = [];
   isDj: boolean = false;
   url: string;
-  tempSong: Song;
+  tempArtist: string;
+  tempSong: string;
   accessKey: string;
-  existingParties: Party[];
-  uniqueSong: boolean;
-  emptySong: boolean;
-  checkVoted;
+  existingParties: Party[] = this.apiService.getParties();
 
-  // Initialize variables
   ngOnInit() {
-    this.getVoteCheck();
-    this.tempSong = new Song();
-    this.uniqueSong = false;
-    this.emptySong = false;
-    this.getParties();
+
+    this.partyName = this.route.snapshot.paramMap.get('name');
+    this.partyid = +this.route.snapshot.paramMap.get('id');
+
+    for (let i = 0; i < this.existingParties.length; i++) // need to modify to use getParties from api.service.ts
+    {
+      if (this.existingParties[i].id == this.partyid && this.existingParties[i].name == this.partyName)
+      {
+        this.accessKey = this.existingParties[i].accessKey;
+      }
+    }
+
+
+    // this.songs = SONGS;
+
+    this.songs = this.songService.getSongs(this.partyid);
+
+    this.songs = this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
+
+    
+    //this.songs.OrderBy(x=>x.upcount);
     this.url = this.router.url;
     if(this.url[1] == 'd')
         this.isDj = true;
@@ -41,171 +63,60 @@ export class PlaylistComponent implements OnInit {
         this.isDj = false;
   }
 
-  // Get checkVoted from session on server side
-  getVoteCheck(){
-    this.songService.getVoteCheck().then(result =>{
-      this.checkVoted = result;
-    })
-  }
-
-  // Use apiService to get parties and initialize party information
-  getParties(){
-    this.apiService.getParties().then(result=> {
-      this.existingParties = result;
-      this.partyName = this.route.snapshot.paramMap.get('name');
-      this.partyid = +this.route.snapshot.paramMap.get('id');
-
-      for (let i = 0; i < this.existingParties.length; i++) // need to modify to use getParties from api.service.ts
-      {
-        if (this.existingParties[i].id == this.partyid && this.existingParties[i].name == this.partyName)
-        {
-          this.accessKey = this.existingParties[i].accessKey;
-        }
-      }
-
-      this.getSongs(this.partyid);
-    })
-  }
-
-  // Use songService to get songs for the party
-  getSongs(partyid: number){
-    this.songService.getSongs(partyid).then(result=>{
-      this.songs = result;
-
-      // Temp variable to keep track of songs in checkVoted that may have been deleted
-      var temp = {};
-      for(var key in this.checkVoted){
-        temp[key] = {};
-        for(var key2 in this.checkVoted[key])
-        {
-          temp[key][key2] = false;
-        }
-      }
-
-      // Add any new songs to checkVoted. Mark songs in temp.
-      for(let i = 0; i < result.length; i++){
-        if(this.checkVoted[result[i].artist] == undefined){
-          this.checkVoted[result[i].artist] = {};
-          this.checkVoted[result[i].artist][result[i].name] = false;
-          temp[result[i].artist] = {};
-          temp[result[i].artist][result[i].name] = true;
-        }
-        else if(this.checkVoted[result[i].artist][result[i].name] == undefined){
-          this.checkVoted[result[i].artist][result[i].name] = false;
-          temp[result[i].artist][result[i].name] = true;
-        }
-        else
-          temp[result[i].artist][result[i].name] = true;
-      }
-
-      // Look for any unmarked songs in temp and delete keys from checkVoted
-      for(var key in temp){
-        for(var key2 in temp[key]){
-          if(!temp[key][key2])
-          {
-            delete this.checkVoted[key][key2];
-          }
-        }
-        if(Object.keys(this.checkVoted[key]).length == 0)
-        {
-          delete this.checkVoted[key];
-        }
-      }
-
-      // Save checkVoted into session on server side
-      this.songService.saveVoteCheck(this.checkVoted).then(result =>{
-        // Sort songs by number of upcounts
-        this.songs.sort((obj1:Song, obj2:Song) => {
-          if(obj1.upcount > obj2.upcount){
-            return -1;
-          }
-          if(obj1.upcount < obj2.upcount){
-            return 1;
-          }
-          return 0;
-        })
-      })
-    })
-  }
-  
-  // Delete songs
-  deleteParty(){
-    this.apiService.deleteParty(this.partyid).then(result=>{
-      if(result == true)
-        this.router.navigateByUrl('/');
-    });
-  }
-
-  // Use songService to add song to database
   onAddSong() :void {
-    // Create song object
-    this.tempSong = new Song();
-    this.tempSong.artist = (<HTMLInputElement>document.getElementById("artist")).value;
-    this.tempSong.name = (<HTMLInputElement>document.getElementById("song")).value;
-    this.tempSong.partyid = this.partyid;
+    this.tempArtist = (<HTMLInputElement>document.getElementById("artist")).value;
+    this.tempSong = (<HTMLInputElement>document.getElementById("song")).value;
+    
+    // make sure song isn't a duplicate
 
-    // Make sure input isn't empty
-    if(this.tempSong.name == "" || this.tempSong.artist == ""){
-      this.emptySong = true;
-    }
-    else{
-      this.emptySong = false;
-
-      // Check for duplicate song
-      var self = this;
-      this.songService.getSongs(this.partyid).then(songs => {
-        var flag = false;
-        for(let i = 0; i < songs.length; i++){
-          if(songs[i].name == this.tempSong.name && songs[i].artist == this.tempSong.artist){
-            flag = true;
-          }
-        }
-        
-        if(flag){ // If song is a duplicate
-          this.uniqueSong = true;
-        }
-        else{
-          this.uniqueSong = false;
-          // If not duplicate, add song
-          self.songService.addSong(this.tempSong).then(result=>{
-            // Update song list
-            self.getSongs(self.partyid);
-
-            // Reset model for songs
-            self.tempSong = new Song();
-          });
-        }
-      });
-    }
+    this.songs.push({ name: this.tempSong, artist: this.tempArtist, partyid: this.partyid, upcount:0 });
    }
 
-  //  Delete song from database
   onSelectDelete(song: Song): void {
-    var self = this;
-    this.songService.deleteSong(song).then(result=>{
-      // Update song list
-      self.getSongs(self.partyid);
-    });
+    for (let i = 0; i < this.songs.length; i++)
+    {
+      if (song.partyid == this.partyid && this.songs[i].name == song.name && this.songs[i].artist == song.artist)
+      {
+        this.songs.splice(i, 1);
+        break;
+      }
+    }
   }
 
-  // Upvote
   onSelectUpVote(song:Song) :void {
-    var self = this;
-    this.checkVoted[song.artist][song.name] = true;
-    this.songService.upvoteSong(song).then(result=>{
-      // Update song list
-      self.getSongs(self.partyid);
-    });
-  }
 
-  // Downvote
+    song.upcount++;
+    this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
+  }
   onSelectDownVote(song:Song) :void {
-    var self = this;
-    this.checkVoted[song.artist][song.name] = true;    
-    this.songService.downvoteSong(song).then(result=>{
-      // Update song list
-      self.getSongs(self.partyid);
-    });
+  	song.upcount--;
+    this.songs.sort((obj1:Song, obj2:Song) => {
+    if(obj1.upcount > obj2.upcount){
+      return -1;
+    }
+    if(obj1.upcount < obj2.upcount){
+      return 1;
+    }
+    return 0;
+  });
   }
 
+//   sort((obj1:Song, obj2:Song) => {
+//     if(obj1.upcount > obj2.upcount){
+//       return 1;
+//     }
+//     if(obj1.upcount < obj2.upcount){
+//       return -1;
+//     }
+//     return 0;
+//   });
+// }
 }
